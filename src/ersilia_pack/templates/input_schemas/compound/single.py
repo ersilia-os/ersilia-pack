b5977166f86185d1c273fcc8a425ca6d294f19c2
Malik Badmus <malik.badmus@icloud.com>
2025-01-08 02:13:54 +0100

    changing filename

--- src/ersilia_pack/templates/input_schemas/compound/single.py
+++ src/ersilia_pack/templates/input_schemas/compound/single.py
@@ -5,12 +5,29 @@ from typing import List, Dict, Union
 
 root = os.path.dirname(os.path.abspath(__file__))
 
+
 def read_example():
-    with open(os.path.join(root, "..", "model", "framework", "examples", "input.csv"), "r") as f:
+    """
+    Read example input data.
+    """
+    file_to_open = None
+
+    for filename in ["run_input.csv", "input.csv"]:
+        path = os.path.join(root, "..", "model", "framework", "examples", filename)
+        if os.path.exists(path):
+            file_to_open = path
+            break
+
+    if not file_to_open:
+        raise FileNotFoundError("No example input file found (run_input.csv or input.csv).")
+
+    
+    with open(file_to_open, "r") as f:
         reader = csv.reader(f)
-        next(reader)
+        next(reader)  
         data = [x[0] for x in reader]
-    return data    
+    return data
+   
 
 
 exemplary_input = read_example()[:3]
