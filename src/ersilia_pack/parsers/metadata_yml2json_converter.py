e7c4572849d7c6215a8d4cebde85f98db8ca2f75
GemmaTuron <gemma@ersilia.io>
2025-04-24 16:39:37 +0200

    update yml conversion fix

--- src/ersilia_pack/parsers/metadata_yml2json_converter.py
+++ src/ersilia_pack/parsers/metadata_yml2json_converter.py
@@ -40,88 +40,86 @@ class MetadataYml2JsonConverter:
 
   def convert(self):
     data = collections.OrderedDict()
-    
     if "Identifier" in self.data:
-        data["Identifier"] = self._tostr(self.data["Identifier"])
+      data["Identifier"] = self._tostr(self.data["Identifier"])
     if "Slug" in self.data:
-        data["Slug"] = self._tostr(self.data["Slug"])
+      data["Slug"] = self._tostr(self.data["Slug"])
     if "Status" in self.data:
-        data["Status"] = self._tostr(self.data["Status"])
+      data["Status"] = self._tostr(self.data["Status"])
     if "Title" in self.data:
-        data["Title"] = self._tostr(self.data["Title"])
+      data["Title"] = self._tostr(self.data["Title"])
     if "Description" in self.data:
-        data["Description"] = self._tostr(self.data["Description"])
+      data["Description"] = self._tostr(self.data["Description"])
     if "Deployment" in self.data:
-        data["Deployment"] = self._tolist(self.data["Deployment"])
+      data["Deployment"] = self._tolist(self.data["Deployment"])
     if "Source" in self.data:
-        data["Source"] = self._tostr(self.data["Source"])
+      data["Source"] = self._tostr(self.data["Source"])
     if "Source Type" in self.data:
-        data["Source Type"] = self._tostr(self.data["Source Type"])
+      data["Source Type"] = self._tostr(self.data["Source Type"])
     if "Task" in self.data:
-        data["Task"] = self._tostr(self.data["Task"])
+      data["Task"] = self._tostr(self.data["Task"])
     if "Subtask" in self.data:
-        data["Subtask"] = self._tostr(self.data["Subtask"])
+      data["Subtask"] = self._tostr(self.data["Subtask"])
     if "Input" in self.data:
-        data["Input"] = self._tolist(self.data["Input"])
+      data["Input"] = self._tolist(self.data["Input"])
     if "Input Dimension" in self.data:
-        data["Input Dimension"] = self._toint(self.data["Input Dimension"])
+      data["Input Dimension"] = self._toint(self.data["Input Dimension"])
     if "Output" in self.data:
-        data["Output"] = self._tolist(self.data["Output"])
+      data["Output"] = self._tolist(self.data["Output"])
     if "Output Dimension" in self.data:
-        data["Output Dimension"] = self._toint(self.data["Output Dimension"])
+      data["Output Dimension"] = self._toint(self.data["Output Dimension"])
     if "Output Consistency" in self.data:
-        data["Output Consistency"] = self._tostr(self.data["Output Consistency"])
+      data["Output Consistency"] = self._tostr(self.data["Output Consistency"])
     if "Interpretation" in self.data:
-        data["Interpretation"] = self._tostr(self.data["Interpretation"])
+      data["Interpretation"] = self._tostr(self.data["Interpretation"])
     if "Tag" in self.data:
-        data["Tag"] = self._tolist(self.data["Tag"])
+      data["Tag"] = self._tolist(self.data["Tag"])
     if "Biomedical Area" in self.data:
-        data["Biomedical Area"] = self._tolist(self.data["Biomedical Area"])
+      data["Biomedical Area"] = self._tolist(self.data["Biomedical Area"])
     if "Target Organism" in self.data:
-        data["Target Organism"] = self._tolist(self.data["Target Organism"])
+      data["Target Organism"] = self._tolist(self.data["Target Organism"])
     if "Publication Type" in self.data:
-        data["Publication Type"] = self._tostr(self.data["Publication Type"])
+      data["Publication Type"] = self._tostr(self.data["Publication Type"])
     if "Publication Year" in self.data:
-        data["Publication Year"] = self._toint(self.data["Publication Year"])
+      data["Publication Year"] = self._toint(self.data["Publication Year"])
     if "Publication" in self.data:
-        data["Publication"] = self._tostr(self.data["Publication"])
+      data["Publication"] = self._tostr(self.data["Publication"])
     if "Source Code" in self.data:
-        data["Source Code"] = self._tostr(self.data["Source Code"])
+      data["Source Code"] = self._tostr(self.data["Source Code"])
     if "License" in self.data:
-        data["License"] = self._tostr(self.data["License"])
+      data["License"] = self._tostr(self.data["License"])
     if "Contributor" in self.data:
-        data["Contributor"] = self._tostr(self.data["Contributor"])
+      data["Contributor"] = self._tostr(self.data["Contributor"])
     if "Incorporation Date" in self.data:
-        data["Incorporation Date"] = self._tostr(self.data["Incorporation Date"])
+      data["Incorporation Date"] = self._tostr(self.data["Incorporation Date"])
     if "S3" in self.data:
-        data["S3"] = self._tostr(self.data["S3"])
+      data["S3"] = self._tostr(self.data["S3"])
     if "DockerHub" in self.data:
-        data["DockerHub"] = self._tostr(self.data["DockerHub"])
+      data["DockerHub"] = self._tostr(self.data["DockerHub"])
     if "Docker Architecture" in self.data:
-        data["Docker Architecture"] = self._tolist(self.data["Docker Architecture"])
+      data["Docker Architecture"] = self._tolist(self.data["Docker Architecture"])
     if "Docker Pack Method" in self.data:
-        data["Docker Pack Method"] = self._tostr(self.data["Docker Pack Method"])
+      data["Docker Pack Method"] = self._tostr(self.data["Docker Pack Method"])
     if "Model Size" in self.data:
-        data["Model Size"] = self._tofloat(self.data["Model Size"])
+      data["Model Size"] = self._tofloat(self.data["Model Size"])
     if "Environment Size" in self.data:
-        data["Environment Size"] = self._tofloat(self.data["Environment Size"])
+      data["Environment Size"] = self._tofloat(self.data["Environment Size"])
     if "Image Size" in self.data:
-        data["Image Size"] = self._tofloat(self.data["Image Size"])
+      data["Image Size"] = self._tofloat(self.data["Image Size"])
     if "Computational Performance 4" in self.data:
-        data["Computational Performance 4"] = self._tofloat(self.data["Computational Performance 4"])
+      data["Computational Performance 4"] = self._tofloat(self.data["Computational Performance 4"])
     if "Computational Performance 7" in self.data:
-        data["Computational Performance 7"] = self._tofloat(self.data["Computational Performance 7"])
+      data["Computational Performance 7"] = self._tofloat(self.data["Computational Performance 7"])
     if "Computational Performance 12" in self.data:
-        data["Computational Performance 12"] = self._tofloat(self.data["Computational Performance 12"])
+      data["Computational Performance 12"] = self._tofloat(self.data["Computational Performance 12"])
     if "Computational Performance 20" in self.data:
-        data["Computational Performance 20"] = self._tofloat(self.data["Computational Performance 20"])
+      data["Computational Performance 20"] = self._tofloat(self.data["Computational Performance 20"])
     if "Computational Performance 34" in self.data:
-        data["Computational Performance 34"] = self._tofloat(self.data["Computational Performance 34"])
+      data["Computational Performance 34"] = self._tofloat(self.data["Computational Performance 34"])
     if "Computational Performance 58" in self.data:
-        data["Computational Performance 58"] = self._tofloat(self.data["Computational Performance 58"])
+      data["Computational Performance 58"] = self._tofloat(self.data["Computational Performance 58"])
     if "Computational Performance 100" in self.data:
-        data["Computational Performance 100"] = self._tofloat(self.data["Computational Performance 100"])
-
+      data["Computational Performance 100"] = self._tofloat(self.data["Computational Performance 100"])
     if self.json_file is None:
       return data
     with open(self.json_file, "w") as f:
