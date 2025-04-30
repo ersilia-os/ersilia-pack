33d35bba60ccb0d7537fac483986bb9bae0a77ed
Abellegese <abellegese5@gmail.com>
2025-04-30 15:49:32 +0300

    fix: cloning test model

--- pipeline.sh
+++ pipeline.sh
@@ -1,6 +1,6 @@
 HOME_DIR=$(eval echo ~$USER)
 MODEL_ID="$1"
-
+git clone --depth 1 https://github.com/ersilia-os/eos3b5e.git
 ersilia_model_lint --repo_path "$MODEL_ID"
 ersilia_model_pack --repo_path "$MODEL_ID" --bundles_repo_path "$HOME_DIR"/eos/repository
 ersilia_model_serve --bundle_path "$HOME_DIR"/eos/repository/"$MODEL_ID"
