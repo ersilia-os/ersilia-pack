a0f404acf858d39aeb2cbf2fa50af8d7ae893242
Abellegese <abellegese5@gmail.com>
2025-04-30 17:04:18 +0300

    fix: fixing workflow code

--- pipeline.sh
+++ pipeline.sh
@@ -16,14 +16,6 @@ SERVER_PID=$!
 BASE_URL="http://127.0.0.1:$PORT"
 until curl -sf "$BASE_URL/healthz"; do sleep 1; done
 
-curl --fail \
-     -H "Content-Type: application/json" \
-     -d '[
-       {"key": "0", "input": "Nc1cccc(c1)-c1cc2c(Oc3cccc(O)c3)ncnc2[nH]1"},
-       {"key": "1", "input": "C1=CN=CC=C1C(=O)NN"},
-       {"key": "2", "input": "CC(CN1C=NC2=C(N=CN=C21)N)OCP(=O)(O)O"}
-     ]' \
-     "$BASE_URL/run" \
-  && kill $SERVER_PID || { kill $SERVER_PID; exit 1; }
+kill $SERVER_PID || { kill $SERVER_PID; exit 1; }
 
 wait $SERVER_PID 2>/dev/null
